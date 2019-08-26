73666986905f12c75464f1693e30d3c1f111d19c
Karuma Lin <qscwdvefb3@gmail.com>
2019-08-26 19:19:31 +0800

    update

--- app.py
+++ app.py
@@ -47,7 +47,10 @@ def handle_message(event):
             line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=re))
+            return
         else:
+            line_bot_api.reply_message(
+            event.reply_token)
             return
 
     elif talk == "說話":
